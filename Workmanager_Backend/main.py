6ce0684753204fa1c372645e2b4392d826e64b06
grumpyp <info@gerryontour.de>
2021-01-03 17:34:54 +0100

    gitignore

--- Workmanager_Backend/main.py
+++ Workmanager_Backend/main.py
@@ -5,10 +5,6 @@ from flask_cors import CORS
 import json
 
 
-
-
-
-
 if __name__ == "__main__":
     app = Flask(__name__)
     CORS(app, supports_credentials=True) # used to test the application locally in connection with a web browser
@@ -26,6 +22,18 @@ if __name__ == "__main__":
             print(e)
         return jsonify("0")
 
+
+    @app.route('/daily', methods=["GET", "POST"])
+    def get_day(time):
+        today = []
+        aufgaben = {"5" : ["putzen","prio"], "2" : ["putzen","non_prio"], "3" : ["putzen","non_prio"], "6" : ["putzen","non_prio"], "7" : ["putzen","non_prio"] }
+        time_aufgaben_gesamt = []
+        while time > sum(time_aufgaben_gesamt):
+            for time_needed,aufgabe in aufgaben.items():
+                if "prio" in aufgabe and time > time_needed:
+                    today.append(aufgabe)
+                    time_aufgaben_gesamt.append(time)
+                elif
 
 
 
